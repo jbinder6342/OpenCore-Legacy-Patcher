d0e621deac515c46a61c660d5bb59e66a4c157d8
Jim Bo <bosephus61@gmail.com>
2024-07-04 20:53:34 -0400

    Fixed issue with missing statemewnt in constants.py due to merge.

--- opencore_legacy_patcher/constants.py
+++ opencore_legacy_patcher/constants.py
@@ -13,7 +13,8 @@ from .detections import device_probe
 class Constants:
     def __init__(self) -> None:
         # Patcher Versioning
-        self.patcher_version:                 str = "1.7.99x"  # OpenCore-Legacy-Patcher
+        self.patcher_version:                 str = "1.7.99"  # OpenCore-Legacy-Patcher
+        self.patcher_support_pkg_version:     str = "1.6.1"  # PatcherSupportPkg
         self.copyright_date:                  str = "Copyright © 2020-2024 Dortania"
         self.patcher_name:                    str = "OpenCore Legacy Patcher -- Sequoia Expr build."
 
